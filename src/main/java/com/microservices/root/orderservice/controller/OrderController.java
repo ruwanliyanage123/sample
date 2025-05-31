ef103f9af33dfe61d6e6c27de78d182baca82025
Ruwan <ruwanliyanagegalle1994@gmail.com>
2025-05-31 21:19:36 +0530

    Add modification

--- src/main/java/com/microservices/root/orderservice/controller/OrderController.java
+++ src/main/java/com/microservices/root/orderservice/controller/OrderController.java
@@ -1,5 +1,6 @@
 package com.microservices.root.orderservice.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/order")
 public class OrderController {
+    @Value("${order.url}")
+    private String orderUrl;
     @GetMapping()
     public ResponseEntity<String> createOrder() {
+        for (int i = 0; i < 10; i++){
+            System.out.println("========="+ orderUrl +"============");
+        }
         String message = "--------started---------";
         System.out.println(message);
         return new ResponseEntity<>(message, HttpStatus.OK);
